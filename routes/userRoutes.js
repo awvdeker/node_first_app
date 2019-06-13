506dc86e0db53571182c0e74477a087cad2128f8
awvdeker <alexander.vandekerckhove@gmail.com>
2019-06-13 12:31:54 +0200

    PUT works!

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -54,5 +54,30 @@ module.exports = app => {
     });
   });
 
+  app.put("/user", (req, res) => {
+    let user;
+    let username = req.body.username;
+    let email    = req.query.email;
+
+    jsonfile.readFile(file_path, function(err, content) {
+      for (var i = content.length - 1; i >= 0; i--) {
+        if (content[i].email === req.query.email) {
+
+          console.log("updated user " + req.query.email + " has now username : " + username);
+
+          user = content[i];
+          user.username = username;
+
+        }
+      }
+
+      jsonfile.writeFile(file_path, content, function(err) {
+        console.log(err);
+      });
+
+    });
+    res.send(user);
+  });
+
 
 };
